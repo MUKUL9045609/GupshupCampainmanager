4c4b0bff9d0527ae3fdecf2c9ef047d0c002f0a8
Mukul9045609 <mukul.s@fortune4.in>
2025-06-21 09:57:20 +0530

    Remove unwanted code

--- Gupshupcampainmanager/Controllers/GupshupController.cs
+++ Gupshupcampainmanager/Controllers/GupshupController.cs
@@ -57,44 +57,6 @@ namespace Gupshupcampainmanager.Controllers
         {
             try
             {
-                //ViewData["Title"] = "Send WhatsApp Message";
-
-                //// Load credentials from configuration
-                //string apiKey = _configuration["Gupshup:ApiKey"];
-                //string partnerAppToken = _configuration["Gupshup:PartnerAppToken"];
-                //string appId = _configuration["Gupshup:AppId"];
-                //string source = _configuration["Gupshup:Source"];
-                //string destination = _configuration["Gupshup:Destination"];
-                //string appName = _configuration["Gupshup:AppName"];
-                //string templateId = _configuration["Gupshup:TemplateId"];
-
-                //// Validate credentials
-                //if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(partnerAppToken) || string.IsNullOrEmpty(appId) ||
-                //    string.IsNullOrEmpty(source) || string.IsNullOrEmpty(destination) || string.IsNullOrEmpty(appName) ||
-                //    string.IsNullOrEmpty(templateId))
-                //{
-                //    _logger.LogError("One or more Gupshup configuration values are missing at {Time}", DateTime.Now);
-                //    ViewBag.ResponseMessage = "Configuration error: Missing Gupshup credentials.";
-                //    ViewBag.AlertClass = "alert-danger";
-                //    return View();
-                //}
-
-                //if (imageFile == null || imageFile.Length == 0)
-                //{
-                //    ViewBag.ResponseMessage = "Please upload an image.";
-                //    ViewBag.AlertClass = "alert-danger";
-                //    return View();
-                //}
-
-                //if (string.IsNullOrEmpty(offerText))
-                //{
-                //    ViewBag.ResponseMessage = "Offer text is required.";
-                //    ViewBag.AlertClass = "alert-danger";
-                //    return View();
-                //}
-
-                //string imageHandleId = await _gupshupApiService.UploadImageToGupshup(partnerAppToken, appId, imageFile);
-
                 string result = await _gupshupApiService.SendWhatsAppMessage("","","", "", "", "","",""); ;
 
                 ViewBag.ResponseMessage = "Message sent successfully! ";
