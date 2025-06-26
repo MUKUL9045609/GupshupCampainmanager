6dc3833d3885d92570d86729149b4dcbc1590dae
Mukul9045609 <mukul.s@fortune4.in>
2025-06-26 17:51:41 +0530

    Remove Description field

--- Gupshupcampainmanager/Models/WhatsAppRecipient.cs
+++ Gupshupcampainmanager/Models/WhatsAppRecipient.cs
@@ -7,14 +7,12 @@ namespace Gupshupcampaignmanager.Models
     {
         public long? phone { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
     }
     public class WhatsAppRecipientMap : ClassMap<WhatsAppRecipient>
     {
         public WhatsAppRecipientMap()
         {
             Map(m => m.Name);
-            Map(m => m.Description);
             Map(m => m.phone).TypeConverter<PhoneNumberConverter>(); // Apply custom converter here
         }
     }
